fd725aadd99a500de0fee3685b9998f6759c904c
Alexander Cuenca <alex.charco.25@gmail.com>
2022-12-24 10:40:57 -0500

    New version of function:

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,51 +9,77 @@ const GMAIL_PASS = defineSecret("GMAIL_PASS");
 
 initializeApp();
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: GMAIL_ACCOUNT.value(),
-    pass: GMAIL_PASS.value(),
-  },
-});
+let transporter: any = null;
 
-exports.sendMail = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    const destinationEmail = req.query.dest as string;
-
-    if (
-      destinationEmail === null ||
-      destinationEmail === undefined ||
-      destinationEmail.length === 0
-    ) {
-      return;
-    }
-
-    if (!isValidEmail(destinationEmail)) {
-      return;
-    }
-
-    const mailOptions = {
-      from: GMAIL_ACCOUNT.value(),
-      to: destinationEmail,
-      subject: "Hello!!!",
-      html: `<h3 style="font-size: 16px;">Thanks for your request!!</h3>
-                <p>I will responde you as soon as I read your email and requirement. Until then enjoy this image!</p>
-                <br />
-                <img src="https://images.prod.meredith.com/product/fc8754735c8a9b4aebb786278e7265a5/1538025388228/l/rick-and-morty-pickle-rick-sticker" />
-            `,
-    };
-
-    return transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        return res.send(error.toString());
+export const sendMail = functions
+  .runWith({ secrets: [GMAIL_ACCOUNT, GMAIL_PASS] })
+  .https.onRequest((req, res) => {
+    cors(req, res, () => {
+      const method = req.method;
+      if (method !== "POST") {
+        return res.send("Invalid method.");
       }
-      return res.send("Email sent, I'll be in touch soon!!!");
+      
+      const { name, email, message } = req.body;
+
+      if (
+        !checkPresentParameter(name) ||
+        !checkPresentParameter(email) ||
+        !checkPresentParameter(message)
+      ) {
+        return res.send("Fill all the parameters.");
+      }
+
+      if (!isValidEmail(email)) {
+        return res.send("Insert a valid email.");
+      }
+
+      transporterLazyLoad(
+        transporter,
+        GMAIL_ACCOUNT.value(),
+        GMAIL_PASS.value()
+      );
+
+      const mailOptions = {
+        from: GMAIL_ACCOUNT.value(),
+        to: email,
+        subject: "New contact!!!",
+        html: `<h3 style="font-size: 16px;">Portfolio Contact!</h3>
+                <p>Name: ${name}</p>
+                <br />
+                <p>Email: ${email}</p>
+                <br />
+                <p>Message: ${message}</p>
+                <br />
+            `,
+      };
+
+      return transporter.sendMail(mailOptions, (error: { toString: () => any; }, info: any) => {
+        if (error) {
+          return res.send(error.toString()  );
+        }
+        return res.send("Email sent, I'll be in touch soon!!!");
+      });
     });
   });
-});
 
 const isValidEmail = (email: string): boolean => {
   const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
   return expression.test(email);
+};
+
+const checkPresentParameter = (val?: string) => {
+  return val === null || val === undefined || val.length === 0;
+};
+
+const transporterLazyLoad = (transporter: any, email: string, pass: string) => {
+  if (transporter === null) {
+    transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: email,
+        pass: pass,
+      },
+    });
+  }
 };
