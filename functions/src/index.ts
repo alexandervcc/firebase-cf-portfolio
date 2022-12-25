3c2d35d29665549af5fa63a450f4a8437d4575e5
Alexander Cuenca <alex.charco.25@gmail.com>
2022-12-24 20:52:04 -0500

    Final version

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,17 +7,15 @@ const cors = require("cors")({ origin: true });
 
 const GMAIL_ACCOUNT = defineSecret("GMAIL_ACCOUNT");
 const GMAIL_PASS = defineSecret("GMAIL_PASS");
+const GMAIL_DEST = defineSecret("GMAIL_DEST");
 
 initializeApp();
 
-let transporter: any = null;
-
 export const sendMail = functions
-  .runWith({ secrets: [GMAIL_ACCOUNT, GMAIL_PASS] })
+  .runWith({ secrets: [GMAIL_ACCOUNT, GMAIL_PASS, GMAIL_DEST] })
   .https.onRequest((req, res) => {
     cors(req, res, () => {
-      const method = req.method;
-      if (method !== "POST") {
+      if (req.method !== "POST") {
         return res.status(405).send({ error: "Method not allowed." });
       }
 
@@ -35,43 +33,36 @@ export const sendMail = functions
         return res.status(400).send({ error: "Insert a valid email." });
       }
 
-      try {
-        transporterLazyLoad(
-          transporter,
-          GMAIL_ACCOUNT.value(),
-          GMAIL_PASS.value()
-        );
-      } catch (error) {
-        return res.status(500).send({ error: error });
-      }
-
       const mailOptions = {
         from: GMAIL_ACCOUNT.value(),
-        to: GMAIL_ACCOUNT.value(),
-        subject: "New contact!!!",
-        html: `<h3 style="font-size: 16px;">Portfolio Contact!</h3>
+        to: GMAIL_DEST.value(),
+        subject: "Portfolio Contact Request",
+        html: `<h3>New Portfolio Contact!!!</h3>
                 <p>Name: ${body.name}</p>
-                <br />
                 <p>Email: ${body.email}</p>
-                <br />
                 <p>Message: ${body.message}</p>
-                <br />
             `,
       };
 
-      return transporter.sendMail(
-        mailOptions,
-        (error: { toString: () => any }, info: any) => {
-          if (error) {
-            return res
-              .status(501)
-              .send({ error: error.toString(), error_message: info });
-          }
-          return res
-            .status(200)
-            .send({ info: "Email sent, I'll be in touch soon!!!" });
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: GMAIL_ACCOUNT.value(),
+          pass: GMAIL_PASS.value(),
+        },
+      });
+
+      return transporter.sendMail(mailOptions, (error: any, info: any) => {
+        if (error) {
+          res.status(500).send({
+            error: "Something went wrong :(, please try again later. ",
+          });
+        } else {
+          res.status(300).send({
+            res: "Thanks for your message, I will contact you soon!!!.",
+          });
         }
-      );
+      });
     });
   });
 
@@ -82,16 +73,4 @@ const isValidEmail = (email: string): boolean => {
 
 const valueIsEmpty = (val: string) => {
   return val === null || val === undefined;
-};
-
-const transporterLazyLoad = (transporter: any, email: string, pass: string) => {
-  if (transporter === null) {
-    transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: email,
-        pass: pass,
-      },
-    });
-  }
 };
